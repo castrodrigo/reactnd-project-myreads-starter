8b0044885326cc535e8e69e040561a25ab0a7592
castrodrigo <castrodrigo@yahoo.com.br>
2019-01-01 23:26:48 +0100

    Handle Api Integration when App Loads to fetch all books

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from '../api/BooksAPI';
 import Search from './Search';
 import List from './List';
 import './App.css';
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  };
+  componentDidMount() {
+    BooksAPI.getAll().then(books => this.setState({ books }));
+  }
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={List} />
-        <Route path="/search" component={Search} />
+        <Route
+          exact
+          path="/"
+          render={() => <List books={this.state.books} />}
+        />
+        <Route
+          path="/search"
+          render={() => <Search books={this.state.books} />}
+        />
       </div>
     );
   }
