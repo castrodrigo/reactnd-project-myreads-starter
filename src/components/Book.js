55f3377ebd41639bdc1b1eb571922a1b8b7edb98
castrodrigo <castrodrigo@yahoo.com.br>
2019-01-04 00:06:16 +0100

    Process Authors and improve prop validation

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import BookCover from './BookCover';
 import BookOptions from './BookOptions';
 
@@ -10,9 +11,25 @@ const Book = ({ book }) => (
         <BookOptions shelf={book.shelf} />
       </div>
       <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.author}</div>
+      <div className="book-authors">
+        {book.authors
+          .reduce((authors, author) => {
+            authors = `${authors} | ${author}`;
+            return authors;
+          }, '')
+          .slice(3)}
+      </div>
     </div>
   </li>
 );
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    imageLinks: PropTypes.object.isRequired,
+    shelf: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    authors: PropTypes.array.isRequired
+  }).isRequired
+};
 
 export default Book;
