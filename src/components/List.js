5ff726e2eeff08bb3d664c2693d2552f9bab41fe
castrodrigo <castrodrigo@yahoo.com.br>
2019-01-04 00:05:34 +0100

    Break list into filtered BookShelf, rendered through shelves prop

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import BookShelf from './BookShelf';
 
-const List = ({ books }) => (
+const List = ({ books, shelves }) => (
   <div className="list-books">
     <div className="list-books-title">
       <h1>MyReads</h1>
     </div>
     <div className="list-books-content">
       <div>
-        <BookShelf name="Currently Reading" books={books} />
-        <BookShelf name="Want to Read" books={books} />
-        <BookShelf name="Read" books={books} />
+        {shelves.map(shelf => (
+          <BookShelf
+            key={shelf.type}
+            shelf={shelf.type}
+            name={shelf.name}
+            books={books}
+          />
+        ))}
       </div>
     </div>
     <div className="open-search">
@@ -19,5 +25,10 @@ const List = ({ books }) => (
     </div>
   </div>
 );
+
+List.propTypes = {
+  books: PropTypes.array.isRequired,
+  shelves: PropTypes.array.isRequired
+};
 
 export default List;
