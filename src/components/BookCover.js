60eabec42eeda2cc5b8133eeaab564f5fd08c846
castrodrigo <castrodrigo@yahoo.com.br>
2019-01-05 23:59:47 +0100

    Simplify Book Cover

--- src/components/BookCover.js
+++ src/components/BookCover.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const BookCover = ({ imageLinks, height, width }) => {
-  let style = {
-    width: width || 128,
-    height: height || 193
-  };
+  let style = { width, height };
   if (imageLinks.thumbnail) {
     style.backgroundImage = `url(${imageLinks.thumbnail})`;
   }
   return <div className="book-cover" style={style} />;
+};
+
+BookCover.propTypes = {
+  imageLinks: PropTypes.shape({
+    thumbnail: PropTypes.string
+  }),
+  width: PropTypes.number,
+  height: PropTypes.number
+};
+
+BookCover.defaultProps = {
+  width: 128,
+  height: 193
 };
 
 export default BookCover;
